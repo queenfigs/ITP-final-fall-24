317279efb892f38fd89690bb02f293e803a819f4
queenfigs <cjn1@evergreen.edu>
2024-12-01 17:17:38 -0800

    added skeleton

--- FinalProject.py
+++ FinalProject.py
@@ -3,21 +3,51 @@
 # Introduction to Programming: Python I at Evergreen State College Fall 2024.
 # Final Project: Task Manager Application
 
-def main():
-  print("Welcome to the Task Manager Application!")
-
 def menu():
   items = ["Add Task", "Mark Tasks as Complete", "View Tasks", "Delete Task", "Filter Tasks", "Exit"]
-  print("1. Add Task")
+  counter = 1
+  for item in items:
+    print(f"{counter}. {item}")
+    counter += 1
+
+def addTask():
+  print("add task")
+  
+def completeTask():
+  print("completeTask")
+  
+def viewTasks():
+  print("lookem")
+  
+def deleteTask():
+  print("del")
+  
+def filterTasks():
+  print("filter")
+    
 
 
 
 
 
-
-
-
-
-
-
-main()
+# main loop
+print("Welcome to the Task Manager Application!\n")
+menu()
+done = False
+while not done:  
+  choice = input("\nPlease enter your choice: ")
+  if choice == "1":
+    addTask()
+  elif choice == "2":
+    completeTask()
+  elif choice == "3":
+    viewTasks()
+  elif choice == "4":
+    deleteTask()
+  elif choice == "5":
+    filterTasks()
+  elif choice == "6":
+    print("Thank you for using the Task Manager Application!")
+    done = True
+  else:
+    menu()
